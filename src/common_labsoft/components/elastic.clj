ac1d2761ebd4c754fe541c764075db7aac21bc3b
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-19 21:21:57 -0200

    Elastic component

--- src/common_labsoft/components/elastic.clj
+++ src/common_labsoft/components/elastic.clj
@@ -1,1 +1,15 @@
-(ns common-labsoft.components.elastic)
+(ns common-labsoft.components.elastic
+  (:require [com.stuartsierra.component :as component]
+            [clojurewerkz.elastisch.rest :as esr]
+            [common-labsoft.protocols.config :as protocols.config]))
+
+(defrecord Elastic [config]
+  component/Lifecycle
+  (start [this]
+    (let [host (protocols.config/get! config :elastic-endpoint)]
+      (assoc this :conn (esr/connect host))))
+  (stop [this]
+    (dissoc this :conn)))
+
+(defn new-elastic []
+  (map->Elastic {}))
